8f85ca4ff13ed108e442b459878dd4676f305047
kimberlybone <k_bone@uncg.edu>
2020-02-09 17:46:57 -0500

    added parameter and a decorator locally

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 import { addDecorator } from '@storybook/react'
+import { load, addParameters } from '@storybook/react';
+import defaultNotes from './instructions.md';
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [storyFn => <div style={{ backgroundColor: 'yellow' }}>{storyFn()}</div>],
 };
+
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
@@ -17,5 +21,10 @@ export const Emoji = () => (
     </span>
   </Button>
 );
+Emoji.story = {
+  decorators: [storyFn => <div style={{ border: '5px solid red'}}>{storyFn()}</div>]
+}
 
-addDecorator( storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>) 
+
+addDecorator( storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>)
+addParameters({ notes: defaultNotes });
