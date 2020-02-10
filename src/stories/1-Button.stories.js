9ef3d47d6835b97f958ddc3e90a5d269fd2d786c
kimberlybone <k_bone@uncg.edu>
2020-02-10 00:05:31 -0500

    changed styling

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -2,27 +2,25 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 import { addDecorator } from '@storybook/react'
-import { load, addParameters } from '@storybook/react';
-// import defaultNotes from './instructions.md';
 
 export default {
   title: 'Button',
   component: Button,
-  decorators: [storyFn => <div style={{ backgroundColor: 'yellow' }}>{storyFn()}</div>],
+  decorators: [storyFn => <div style={{ backgroundColor: 'pink' }}>{storyFn()}</div>],
 };
 
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button onClick={action('Cha-ching!')}>
     <span role="img" aria-label="so cool">
-      😀 😎 👍 💯💎
+      💎💎💎
     </span>
   </Button>
 );
 Emoji.story = {
-  decorators: [storyFn => <div style={{ border: '5px solid red'}}>{storyFn()}</div>]
+  decorators: [storyFn => <div style={{ border: '5px solid purple'}}>{storyFn()}</div>]
 }
 
 
